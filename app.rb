4a42853b2837968ce4cc63e2c5e568690ae7dcba
Yomaira Escano <yjescano@gmail.com>
2018-01-23 16:13:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -15,11 +15,12 @@ class App < Sinatra::Base
   end
 
   get '/first_exercise' do
-    "Your first exercise will be to set your session key-value pair.\nIn the route: get '/set', write a line of code that sets the :foo key of the session hash equal to 'hello'.\nThen, naviate to the '/set' path."
+    "Your first exercise will be to set your session key-value pair.\nIn the route: get '/set', write a line of code that sets the :foo key of the session hash equal to 'hello'.\nThen, navigate to the '/set' path."
   end
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
+    session["foo"] = "hello"
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
@@ -37,7 +38,7 @@ class App < Sinatra::Base
 
   get '/set_session' do
     #set session id here
-
+    session["id"] = 1
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
       redirect '/fetch_session_id'
@@ -52,6 +53,7 @@ class App < Sinatra::Base
 
   get '/logout' do
     #clear session hash here
+    session.clear
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
